bdc943d8d81d261c11166a0d7dc190de99aea5b6
Furesz Peter <fureszpeter@gmail.com>
2015-07-29 22:28:50 +0100

    SQUASH cli-config

--- templates/cli-config.php
+++ templates/cli-config.php
@@ -1,9 +1,9 @@
 <?php
-use Doctrine\ORM\Tools\Console\ConsoleRunner;
-use Illuminate\Foundation\Application;
+//use Doctrine\ORM\Tools\Console\ConsoleRunner;
+//use Illuminate\Foundation\Application;
 
-/** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
+///** @var Application $app */
+//$app = require_once __DIR__.'/../bootstrap/app.php';
 
 // replace with mechanism to retrieve EntityManager in your app
 //$entityManager = GetEntityManager();
