0b8daf9556f81476a42d8671ff066d85d2af62b5
Furesz Peter <fureszpeter@gmail.com>
2015-07-29 22:27:20 +0100

    Add config tempaltes

---
+++ templates/cli-config.php
@@ -0,0 +1,11 @@
+<?php
+use Doctrine\ORM\Tools\Console\ConsoleRunner;
+use Illuminate\Foundation\Application;
+
+/** @var Application $app */
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+// replace with mechanism to retrieve EntityManager in your app
+//$entityManager = GetEntityManager();
+
+//return ConsoleRunner::createHelperSet($entityManager);
